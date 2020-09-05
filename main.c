dfa4811fb06049f17a835f24e0b35ad28100e188
panagiotislivanis <52828714+panagiotislivanis@users.noreply.github.com>
2020-09-05 17:01:25 +0300

    v1.1

--- main.c
+++ main.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 void login();
 int main(int argc, char *argv[]) {
 	int menu;
-	//FILE *fp;
 	printf("menu\n");
 	printf("1.login\n");
 	printf("2.sign up\n");
@@ -16,49 +15,36 @@ int main(int argc, char *argv[]) {
 		login ();	}    
 	else if (menu==2){	
 		printf("17\n");}
-    //fp =fopen ("test.txt","r+");  
-	//if (fp==NULL){
-	//	printf("error");
-	//	exit(0);
-	//		}	
-	//fprintf(fp,"\n");
-	//fprintf(fp,"hello\n");
-	//fclose (fp);
 	return 0;}
 
 void login(){
 	int flag = 0;
+    char onoma[100],kodikos,temp [100];
 	printf("32 \n");
 	FILE *fp;
-	fp =fopen ("test.txt","r+");
+	fp =fopen ("name.txt","r+");
 	if (fp==NULL){
 		printf("error \n");
 		exit(0);
-	}
-	printf("39 \n");	
-	char onoma[100],kodikos,temp [100];
-	int rest;
+	    }
+	printf("39 \n");
 	printf("dose onoma\n");
 	scanf("%s",onoma);
 	printf("to onoma einai %s ",onoma);
 	while (!feof(fp)){
 		printf("46\n");
-	    fscanf(fp, "%s", temp);
-	    printf("%c",temp);
+		 fscanf(fp, "%s", &temp);
+		   printf("%c",temp);
         if(strcmp(temp, onoma) == 0){
         	printf("User with username %s exist!\n", temp);
-        	flag = 1;
-        	break;
+        	 flag = 1;
+        	  break;
 		}
-        /*printf("%s",temp);
-        printf("%d\n",rest);
-	    if (rest==0){
-	    	printf("hello90");
-	    } */
 	}
 	fclose(fp);
 	if(flag == 0)
 		printf("User with username %s not exist!\n", onoma);
 	printf("53");
+
 }
 
